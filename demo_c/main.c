85c04896fc152a69fb6bb8ff262244534b1e6762
Steamed-Bread <shuvictor@163.com>
2018-08-11 22:28:56 +0800

    Initial Commit

---
+++ demo_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  demo_c
+//
+//  Created by 徐星 on 2018/8/11.
+//  Copyright © 2018年 徐星. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
