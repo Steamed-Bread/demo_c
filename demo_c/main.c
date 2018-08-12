04bb4a6ca4a14bafa4cbcb42767972bbddc6bf31
Steamed-Bread <shuvictor@163.com>
2018-08-12 22:59:59 +0800

    if switch label

--- demo_c/main.c
+++ demo_c/main.c
@@ -7,12 +7,16 @@
 //
 
 #include <stdio.h>
-//常量定义
+//常量定义 一般为大写
 #define MY_AGE 100
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
 //编译时
 const int i=10000;
 //运行时
-
+typedef int32_t xuxing;
 int main(int argc, const char * argv[]) {
     // insert code here...
 // ------------整型--------------------
@@ -51,31 +55,84 @@ int main(int argc, const char * argv[]) {
 ////    a=3.000000,b=3.000000,c=0.500000
     
  // ------------字符--------------------
-        printf("hello\nworld\n");
-        printf("hello\rworld\n");
-//    回车符
-        printf("hello\bworld\n");
-//    回退符
-        printf("hello\tworld\n");
-//    制表符
+//        printf("hello\nworld\n");
+//        printf("hello\rworld\n");
+////    回车符
+//        printf("hello\bworld\n");
+////    回退符
+//        printf("hello\tworld\n");
+////    制表符
+//
+////    hello
+////    world
+////    world
+////    hellworld
+////    hello    world
+//
+//      printf("length of char %d\n",sizeof(char));
+////    字符为一个字节
+//    char ch='a';
+//     printf("char %d\n",ch);
+//     printf("char %c\n",ch);
+//     printf("char %c\n",ch-32);
+////    char 97
+////    char a
+////    char A
+//     printf("char %s\n","aaaa");
     
-//    hello
-//    world
-//    world
-//    hellworld
-//    hello    world
     
-      printf("length of char %d\n",sizeof(char));
-//    字符为一个字节
-    char ch='a';
-     printf("char %d\n",ch);
-     printf("char %c\n",ch);
-     printf("char %c\n",ch-32);
-//    char 97
-//    char a
-//    char A
-     printf("char %s\n","aaaa");
-    return 0;
+ // ------------自定义数据类型--------------------
+//    xuxing a=1000;
+//    printf("自定义 %d\n",a);
+//    return 0;
+  
+ // ------------if--------------------
+//    int i=88;
+//    if (i>85) {
+//        printf("优秀\n");
+//    } else if(i>60) {
+//        printf("及格\n");
+//    } else {
+//        printf("不及格\n");
+//    }
+  // ------------switch--------------------
+//    int s=UP;
+//    switch (s) {
+//        case UP:
+//             printf("UP\n");
+//            break;
+//        case DOWN:
+//            printf("DOWN\n");
+//            break;
+//        case LEFT:
+//            printf("LEFT\n");
+//            break;
+//        case RIGHT:
+//            printf("RIGHT\n");
+//            break;
+//        default:
+//            printf("NO ANSWER\n");
+//            break;
+//    }
+
+    
+  // ------------goto label--------------------
+    int g=0;
+    g++;
+    if (g<100) {
+        goto label1;
+    }
+
+    label:
+    printf("%d\n",g);
+    printf("%d\n",g);
+    
+    label1:
+    printf("%d\n",g);
+    printf("%d\n",g);
+    
+
+   
 }
 
 
